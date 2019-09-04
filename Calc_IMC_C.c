56df97aaac16be69ead2996b779b82930e996f14
Yuri Sobral <53959412+sobral00@users.noreply.github.com>
2019-09-04 00:03:22 -0300

    Update Calc_IMC_C.c

--- Calc_IMC_C.c
+++ Calc_IMC_C.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Versão 1.0.0
+//Versão 1.1.0
 
 void calc(){ //Cria a função calculo do IMC
 
@@ -9,10 +9,8 @@ void calc(){ //Cria a função calculo do IMC
 
  printf("\nInsira sua altura: \n"); // pede para o usuario inserir a altura
     scanf("%f",&alt); // liga a entrada do usuario a variavel alt
-    setbuf(stdin,NULL);
     printf("Insira seu peso: \n"); // pede para o usuario inserir o peso
     scanf("%f",&peso); // liga a entrada do usuario a variavel peso
-    setbuf(stdin,NULL);
     alt1 = alt*alt; // eleva a altura ao quadrado
     result = peso/alt1; // calcula o IMC
     printf("Seu IMC e %.2f\n",result); //Apresenta o resultado do IMC do usuario
